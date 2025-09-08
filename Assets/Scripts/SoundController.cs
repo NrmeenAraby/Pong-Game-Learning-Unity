fc5ea589bb054366e548012a593596e1e051f072
NrmeenAraby <115955249+NrmeenAraby@users.noreply.github.com>
2025-09-08 04:41:59 +0300

    Added sound to the ball when hits the rackets or walls

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour {
+
+    public AudioSource ballSound;
+    public AudioSource wallSound;
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name=="Player1" || collision.gameObject.name == "Player2")
+        {
+            ballSound.Play();
+        }
+        else
+        {
+            wallSound.Play();
+        }
+    }
+}
